b3f27da7a5fbb8b1f5052481063af3d1bb3b31f8
Mutiara Sabrina <mutiarajauhari1910@gmail.com>
2023-10-08 03:45:42 +0700

    LoginScreen

--- Tugas AndroFundamental/Tugas1/app/src/main/java/com/example/tugas1/RegistrasiActivity.kt
+++ Tugas AndroFundamental/Tugas1/app/src/main/java/com/example/tugas1/RegistrasiActivity.kt
@@ -1,11 +1,26 @@
 package com.example.tugas1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class RegistrasiActivity : AppCompatActivity() {
+class RegistrasiActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrasi)
+
+        val btnRegist: Button = findViewById(R.id.btn_regis)
+        btnRegist.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_regis -> {
+                val intent = Intent(this@RegistrasiActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
