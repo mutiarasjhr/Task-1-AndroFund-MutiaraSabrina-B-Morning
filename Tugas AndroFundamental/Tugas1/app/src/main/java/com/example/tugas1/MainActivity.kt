467dce958610ea5a5bb9cd01bd9e01c2cc4d2c76
Mutiara Sabrina <mutiarajauhari1910@gmail.com>
2023-10-08 15:13:48 +0700

    hampir

--- Tugas AndroFundamental/Tugas1/app/src/main/java/com/example/tugas1/MainActivity.kt
+++ Tugas AndroFundamental/Tugas1/app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.tugas1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.tv_view1)
+
+        val username : String? =intent.getParcelableExtra<User>("User")?.username
+        val password : String? =intent.getParcelableExtra<User>("User")?.password
+        tv.text = "Username: $username dan Password: $password"
+        val btnShare: Button = findViewById(R.id.btn_share)
+        btnShare.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.btn_share -> {
+//                val message = "Haloo Mutiara"
+//                val intent = Intent()
+//                intent.action = Intent.ACTION_SEND
+//                intent.putExtra(Intent.EXTRA_TEXT, message)
+//                intent.type = "text/plain"
+//                startActivity(intent)
+
+                val intent = Intent()
+                intent.putExtra("history", "Anda Sudah Login")
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+        }
+    }
+
 }
--- Tugas AndroFundamental/Tugas1/app/src/main/java/com/example/tugas1/RegistrasiActivity.kt
+++ Tugas AndroFundamental/Tugas1/app/src/main/java/com/example/tugas1/RegistrasiActivity.kt
@@ -5,11 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class RegistrasiActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword : EditText
+    private lateinit var etConfirmPass : EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrasi)
+
+        etUsername = findViewById(R.id.edt_username)
+        etPassword = findViewById(R.id.edt_password)
+        etConfirmPass = findViewById(R.id.edt_fullname)
 
         val btnRegist: Button = findViewById(R.id.btn_regis)
         btnRegist.setOnClickListener(this)
@@ -18,7 +29,13 @@ class RegistrasiActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when(v.id){
             R.id.btn_regis -> {
+
+                val bundle = Bundle()
+                bundle.putString("username", etUsername.text.toString())
+                bundle.putString("password", etPassword.text.toString())
+
                 val intent = Intent(this@RegistrasiActivity, LoginActivity::class.java)
+                intent.putExtras(bundle)
                 startActivity(intent)
             }
         }
